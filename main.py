e0adee36c42a0aae7d7ba90aba829820341c75d4
0-Mayday-0 <mateogreslebin@gmail.com>
2025-09-19 14:19:27 -0300

    Working on Exit/Enter
    Working on syncing Exit/Enter by Whitey as an example

---
+++ main.py
@@ -0,0 +1,53 @@
+from time import sleep as tsleep
+from sets import Line, CharacterSet
+from playsound import playsound
+import threading
+
+def async_sound(path):
+    threading.Thread(target=playsound, args=(path,), daemon=True).start()
+
+def intro():
+    lines: dict[str, list[Line]] = {
+        'clock':[
+            Line([
+                CharacterSet("The sun ", 0.05),
+                CharacterSet("says leave.\n", 0.05),
+                CharacterSet("The clock ", 0.05),
+                CharacterSet("agrees.\n", 0.07),
+            ], 2),
+        ],
+
+        'dreams':[
+            Line([
+                CharacterSet("My dreams ", 0.05),
+                CharacterSet("have left ", 0.05),
+                CharacterSet("ahead ", 0.1),
+                CharacterSet("of me.\n", 0.06),
+                CharacterSet("The mo", 0.07),
+                CharacterSet("ment flew.\n", 0.07),
+                CharacterSet("They al", 0.07),
+                CharacterSet("ways do.\n", 0.05),
+                CharacterSet("Seems one ", 0.05),
+                CharacterSet("is some", 0.07),
+                CharacterSet("times more ", 0.05),
+                CharacterSet("than two.\n", 0.05)
+            ], 2)
+        ]
+    }
+
+    tsleep(20.5)
+
+    for line in lines['clock']:
+        line.print_delay()
+
+    #tsleep(0.001)
+
+    for line in lines['dreams']:
+        line.print_delay()
+
+def main():
+    async_sound("./music/exit-enter.mp3")
+    intro()
+
+if __name__ == '__main__':
+    main()
